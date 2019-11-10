47c2803be3f5f6ffbcaf62baae17f70cbc3c1fee
PratibhaShrestha <pateebha@gmail.com>
2019-11-10 12:39:45 +0100

    Moved logout button from profile to the settings bottomsheet

--- app/src/main/java/no/oslomet/meet/FragmentProfile.java
+++ app/src/main/java/no/oslomet/meet/FragmentProfile.java
@@ -19,7 +19,6 @@ import java.util.ArrayList;
 
 import no.oslomet.meet.Handler.JsonHandler;
 import no.oslomet.meet.Handler.SettingsHandler;
-import no.oslomet.meet.classes.ApiDataResponse;
 import no.oslomet.meet.classes.Hobbies;
 import no.oslomet.meet.classes.Languages;
 import no.oslomet.meet.classes.PostParam;
@@ -91,50 +90,11 @@ public class FragmentProfile extends Fragment {
         getView().findViewById(R.id.editSettings).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               SettingBottomSheetDialog settingsDialog = new SettingBottomSheetDialog();
+                SettingBottomSheetDialog settingsDialog = new SettingBottomSheetDialog();
                 settingsDialog.show(getFragmentManager(), "SettingsBottomSheet");
-               /*Intent intent = new Intent(getActivity(), ActivitySettings.class);
-               startActivity(intent);*/
             }
         });
 
-
-        getView().findViewById(R.id.signOutButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                AsyncTask.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        Api api = new Api();
-
-                        ArrayList<PostParam> pp = new ArrayList<>();
-                        pp.add(new PostParam("id_user", new SettingsHandler().getStringSetting(getContext(), R.string.preference_idUser)));
-                        pp.add(new PostParam("username", new SettingsHandler().getStringSetting(getContext(), R.string.preference_username)));
-                        pp.add(new PostParam("authenticationToken", new SettingsHandler().getStringSetting(getContext(), R.string.preference_AuthKey)));
-
-                        String jsonData = new JsonHandler()._toJson(pp);
-                        String response = api.GET(Strings.ApiUrl() + "?request=sign_out&data=" + jsonData);
-                        Log.e("Sign out", response);
-                        ApiDataResponse adr = new JsonHandler().getData(response);
-                        if (adr != null && adr.dataExit == 0) {
-                            SettingsHandler sh = new SettingsHandler();
-                            sh.setStringSetting(getContext(), R.string.preference_idUser, "");
-                            sh.setStringSetting(getContext(), R.string.preference_username, "");
-                            sh.setStringSetting(getContext(), R.string.preference_AuthKey, "");
-
-                            Intent i = new Intent(getActivity(), ActivityLaunch.class);
-                            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                            startActivity(i);
-                            //Exit to launcher
-                        }
-
-                    }
-                });
-
-
-            }
-        });
         getIdUser();
         RetrieveUserData();
     }
--- app/src/main/java/no/oslomet/meet/SettingBottomSheetDialog.java
+++ app/src/main/java/no/oslomet/meet/SettingBottomSheetDialog.java
@@ -1,15 +1,26 @@
 package no.oslomet.meet;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetDialogFragment;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import no.oslomet.meet.Handler.JsonHandler;
+import no.oslomet.meet.Handler.SettingsHandler;
+import no.oslomet.meet.classes.ApiDataResponse;
+import no.oslomet.meet.classes.PostParam;
+import no.oslomet.meet.core.Api;
+import no.oslomet.meet.core.Strings;
 
 public class SettingBottomSheetDialog extends BottomSheetDialogFragment {
     @Nullable
@@ -35,6 +46,44 @@ public class SettingBottomSheetDialog extends BottomSheetDialogFragment {
                 getActivity().startActivity(privacyIntent);
             }
         });
+
+        TextView txtLogout = v.findViewById(R.id.infoTxtLogout);
+        txtLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AsyncTask.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        Api api = new Api();
+
+                        ArrayList<PostParam> pp = new ArrayList<>();
+                        pp.add(new PostParam("id_user", new SettingsHandler().getStringSetting(getContext(), R.string.preference_idUser)));
+                        pp.add(new PostParam("username", new SettingsHandler().getStringSetting(getContext(), R.string.preference_username)));
+                        pp.add(new PostParam("authenticationToken", new SettingsHandler().getStringSetting(getContext(), R.string.preference_AuthKey)));
+
+                        String jsonData = new JsonHandler()._toJson(pp);
+                        String response = api.GET(Strings.ApiUrl() + "?request=sign_out&data=" + jsonData);
+                        Log.e("Sign out", response);
+                        ApiDataResponse adr = new JsonHandler().getData(response);
+                        if (adr != null && adr.dataExit == 0) {
+                            SettingsHandler sh = new SettingsHandler();
+                            sh.setStringSetting(getContext(), R.string.preference_idUser, "");
+                            sh.setStringSetting(getContext(), R.string.preference_username, "");
+                            sh.setStringSetting(getContext(), R.string.preference_AuthKey, "");
+
+                            Intent i = new Intent(getActivity(), ActivityLaunch.class);
+                            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(i);
+                            //Exit to launcher
+                        }
+
+                    }
+                });
+
+            }
+        });
+
+
         return v;
     }
 
